1d1e4e644b468b4ef4f5e184d0710ff3e5b14b08
yealia <97204691+yealia@users.noreply.github.com>
2022-10-15 11:27:07 +0900

    gitExampleJava
    gitExampleJava

---
+++ Main.java
@@ -0,0 +1,13 @@
+package day001;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test01 test01 = Test01.builder().name("신예림").age(13).build();
+		
+		System.out.println(test01);
+	}
+
+}
